073e410f2eaebdcd1ecda9176d21e847878deadd
diabagate <47230250+diabagate@users.noreply.github.com>
2019-03-15 03:10:37 +0000

    Add files via upload

---
+++ EXERCICE/séquence 1/script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<script type="text/javascript">
+		document.write("<p>Du text ecrit en javascript.</p>");
+		alert("Hello world! en javascript");
+	</script>
+</head>
+<body>
+	<p>Du text écrit en html.</p>
+
+	
+</body>
+</html>
